229acd1f02a887fbdca574c1daf59bdce9cb02f6
Bennett <153877155+commandmaster@users.noreply.github.com>
2024-03-19 14:11:28 -0600

    i need to rename scipt to monobehavior

--- engineModules/components/script.js
+++ engineModules/components/script.js
@@ -3,6 +3,11 @@ import ComponentBase from "./componentBase";
 export default class Script extends ComponentBase {
     constructor(engineAPI, componentConfig) {
         super(engineAPI, componentConfig);
-        
+    }
+
+    Preload(){
+        import(componentConfig.scriptPath).then((module) => {
+            this.script = new module.default(engineAPI, componentConfig);
+        });
     }
 }
