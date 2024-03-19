5b1329a64e97f3ec2941291eb71efbb083074b3f
Bennett <153877155+commandmaster@users.noreply.github.com>
2024-03-19 13:59:25 -0600

    done

--- engineModules/gameObject.js
+++ engineModules/gameObject.js
@@ -1,6 +1,22 @@
 export default class GameObject {
     constructor(engineAPI, gameObjectConfig) {
-        
+        this.engineAPI = engineAPI;
+        this.gameObjectConfig = gameObjectConfig;
+
+        this.gameEngine = engineAPI.gameEngine;
+        this.p5 = engineAPI.gameEngine.p5;
+    }
+
+    Preload(){
+        return;
+    }
+
+    Start(){
+        return;
+    }
+
+    Update(){
+        return;
     }
     
 }
